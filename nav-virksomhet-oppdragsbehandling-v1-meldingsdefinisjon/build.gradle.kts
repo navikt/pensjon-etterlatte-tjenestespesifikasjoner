558d28929d8cea13e6d18ace16ed2a8f4a6c4a24
Mads Opheim <mads.opheim@nav.no>
2024-09-06 12:20:08 +0200

    Første oppsett

--- nav-virksomhet-oppdragsbehandling-v1-meldingsdefinisjon/build.gradle.kts
+++ nav-virksomhet-oppdragsbehandling-v1-meldingsdefinisjon/build.gradle.kts
@@ -1,9 +1,21 @@
 plugins {
     id("tjenestespesifikasjoner")
+    id("com.github.bjornvester.xjc") version "1.8.1"
 }
 
 dependencies {
     api(libs.org.glassfish.jaxb.jaxb.runtime)
+    implementation(libs.any.annotate)
 }
 
 description = "nav-virksomhet-oppdragsbehandling-v1-meldingsdefinisjon"
+
+
+xjc {
+    xsdDir.set(file("src/main/"))
+    xjcVersion.set("3.0.2")
+    addCompilationDependencies.set(true)
+    options.set(listOf(
+        "xjcClasspath 'com.gitlab.virtual-machinist:any-annotate:2.1.0"
+    ))
+}
