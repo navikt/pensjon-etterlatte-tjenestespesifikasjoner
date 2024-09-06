fd54f60c5d5739af0c1a0baec672a7559a9437f9
Mads Opheim <mads.opheim@nav.no>
2024-09-06 13:16:14 +0200

    Gir opp å bruke ferdiglagde xjc-plugins. Innfører heller vår eigen kodesnutt for det, som bruker xjc direkte. På den eine sida er det litt vedlikehalds-herk, men det aller meste eg har funne av plugins som kan gjera det her for oss er einmannsverk, og dei blir etter ei stund ikkje oppdater lenger, så da er det like greitt å ta eigarskap til koden sjølv

--- nav-virksomhet-oppdragsbehandling-v1-meldingsdefinisjon/build.gradle.kts
+++ nav-virksomhet-oppdragsbehandling-v1-meldingsdefinisjon/build.gradle.kts
@@ -1,21 +1,62 @@
 plugins {
     id("tjenestespesifikasjoner")
-    id("com.github.bjornvester.xjc") version "1.8.1"
 }
 
+val jaxb: Configuration by configurations.creating
+
+val jaxbVersion = "4.0.2"
+val schemaDir = "src/main/xsd"
+val bindingDir = "src/main/xjb"
+val xjcOutputDir = "${layout.buildDirectory.get()}/generated/sources/xjc/main"
+
+
 dependencies {
-    api(libs.org.glassfish.jaxb.jaxb.runtime)
-    implementation(libs.any.annotate)
+    api("org.glassfish.jaxb:jaxb-runtime:$jaxbVersion")
+    jaxb("com.gitlab.virtual-machinist:any-annotate:2.1.0")
+    jaxb("org.glassfish.jaxb:jaxb-xjc:$jaxbVersion")
+    jaxb("jakarta.xml.bind:jakarta.xml.bind-api:$jaxbVersion")
+    jaxb("org.glassfish.jaxb:jaxb-runtime:$jaxbVersion")
 }
 
 description = "nav-virksomhet-oppdragsbehandling-v1-meldingsdefinisjon"
 
+val createXjcOutputDir by tasks.register("createXjcOutputDir") {
+    doLast {
+        mkdir(xjcOutputDir)
+    }
+}
 
-xjc {
-    xsdDir.set(file("src/main/"))
-    xjcVersion.set("3.0.2")
-    addCompilationDependencies.set(true)
-    options.set(listOf(
-        "xjcClasspath 'com.gitlab.virtual-machinist:any-annotate:2.1.0"
-    ))
+val xjc by tasks.registering(JavaExec::class) {
+    dependsOn(createXjcOutputDir)
+    classpath = jaxb
+    mainClass.set("com.sun.tools.xjc.XJCFacade")
+    args = listOf(
+        "-d",
+        xjcOutputDir,
+        "-b",
+        bindingDir,
+        "-encoding",
+        "UTF-8",
+        "-no-header",
+        "-quiet",
+        "-Xany-annotate",
+        "-extension",
+        schemaDir
+    )
+}
+
+tasks.withType<JavaCompile>().configureEach {
+    dependsOn(xjc)
+}
+
+sourceSets {
+    main {
+        java {
+            srcDirs(
+                files(xjcOutputDir) {
+                    builtBy(xjc)
+                }
+            )
+        }
+    }
 }
